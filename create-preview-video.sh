c713f74f2dc60c6c330f7ab458562327551b1fd8
Carson McManus <carson.mcmanus1@gmail.com>
2017-12-12 08:36:45 -0500

    make create-preview-video.sh executable

--- create-preview-video.sh
+++ create-preview-video.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -r 60 -f image2 -s 160x90 -i preview/preview%d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p -vf "scale=16:-2" preview.mp4
+ffmpeg -r 60 -f image2 -s 160x90 -i visualization/preview/ACGAN/preview%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p -vf "scale=16:-2" preview.mp4
