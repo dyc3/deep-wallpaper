5dd77153ed3c610b3e5fda2668b40dac382a61f4
Carson McManus <carson.mcmanus1@gmail.com>
2018-01-11 13:19:43 -0500

    update parameters for create-preview-video.sh so its upscaled but no interpolation occurs.

--- create-preview-video.sh
+++ create-preview-video.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -r 30 -f image2 -s 128x72 -i visualization/preview/ACGAN/vis_epoch_%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p -vf "scale=16:-2" preview.mp4
+ffmpeg -r 30 -f image2 -i visualization/preview/ACGAN/vis_epoch_%04d.png -s 1024x576 -sws_flags neighbor -vcodec libx264 -crf 25  -pix_fmt yuv420p preview.mp4
